bf79fc64438101a11c7d733bf9c8a059a738f0aa
sheldar <kewin.polok@gmail.com>
2017-08-13 12:18:59 +0200

    Serving static content from public directory

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const path = require('path');
 
 class Server {
     constructor () {
         this.application = express();
 
         this.setViewEngines(this.application);
+        this.setStaticContent(this.application);
         this.setRoutes(this.application);
     }
 
@@ -12,10 +14,22 @@ class Server {
         application.set('view engine', 'pug');
     }
 
+    setStaticContent(application) {
+        application.use('/public', express.static(path.normalize(`${__dirname}/../public`)));
+    }
+
     setRoutes(application) {
         application.get('/', function (request, respone) {
             respone.render('index', { title: 'Server', environment: process.env.NODE_ENV});
-        })
+        });
+
+        application.get('/examples/html', function (request, respone) {
+            respone.sendFile(path.normalize(`${__dirname}/../examples/html.html`));
+        });
+
+        application.get('/examples/html-css', function (request, respone) {
+            respone.sendFile(path.normalize(`${__dirname}/../examples/html-css.html`));
+        });
     }
 
     start() {
