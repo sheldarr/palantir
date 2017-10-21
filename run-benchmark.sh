aa9d9d7edcac88971b1b7db6d4569b4cf076ceb3
sheldar <kewin.polok@gmail.com>
2017-10-21 11:08:09 +0200

    Updated run-benchmark script

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-declare -a frameworks=("angular" "angular2")
-declare -a virtualMachines=("64" "128")
+( cd ./javascript-frameworks-benchmark && yarn start )
+
+declare -a frameworks=("vue")
+declare -a virtualMachines=("16")
+
+# declare -a frameworks=("angularjs" "angular" "mithril" "react" "vue")
+# declare -a virtualMachines=("16" "32" "64" "128" "256")
 
 for framework in "${frameworks[@]}"
 do
-    echo "$framework"
+    echo "$framework framework"
 
     for virtualMachine in "${virtualMachines[@]}"
     do
-        echo "$virtualMachine"
+        echo "$virtualMachine virtual machines"
 
-        node ./chrome-performance-logs-recorder/build/main.js --verbose --url="http://localhost:8080/$framework" --filter-event-names="DrawFrame,Layout,Paint" --duration=60000 --local-storage="virtualMachines:$virtualMachine" --output-filename="$framework_$virtualMachine.json" --trim=10
-        node ./trace-event-format-analyzer/build/main.js --verbose --paths="$framework_$virtualMachine.json" --complete-events="Paint" --duration-events="Layout" --immediate-events="DrawFrame" --output
+        node ./chrome-performance-logs-recorder/build/main.js \
+            --url="http://localhost:8080/$framework" \
+            --output-filename="$framework-$virtualMachine.json"
+            --filter-event-names="DrawFrame,Layout,Paint" \
+            --local-storage="virtualMachines:$virtualMachine" \
+            --duration=6000 \
+            --trim=10 \
+            --verbose 
+            
+        node ./trace-event-format-analyzer/build/main.js \
+            --paths="$framework-$virtualMachine.json" \
+            --complete-events="Paint" \
+            --duration-events="Layout" \
+            --immediate-events="DrawFrame" \
+            --output \
+            --verbose
     done
 done
 
+( cd ./javascript-frameworks-benchmark && yarn stop )
